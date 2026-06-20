004feb799784d75d3ec3a89b34b90d40968cde7e
youssefelbdawy1 <youssefelbdawy1@gmail.com>
2026-06-20 04:58:41 +0300

    Swap bcrypt for bcryptjs to fix Vercel build hang

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User  from'../models/User.js';
 
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt  from'jsonwebtoken';
 
 import asyncwrapper  from'../middlewares/asyncwrapper.js';
