d5c6548597d0ba0ad70b2d367e719e43a74cb938
youssefelbdawy1 <youssefelbdawy1@gmail.com>
2026-06-20 17:37:09 +0300

    fix vercel bug

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    const PORT = process.env.PORT || 5000;
+
+    if (process.env.NODE_ENV !== 'production') {
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    }
   })
   .catch((error) => {
     console.error('Failed to start server:', error.message);
